5da63d41106e472aa2cd806271cf8c6476d6f127
Rohit Khadatkar <rohit@catapulthq.com>
2020-07-17 23:17:21 +0530

    added add method

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    port="3309",
+    user="root",
+    passwd="root",
+    database="crud_project"
+ )
+
+cursor = mydb.cursor()
 
 app = Flask(__name__)
 
 @app.route('/')
 def Index():
-    return render_template("index.html")
+    sql = "SELECT  * FROM employee_details"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    print(data)
+    return render_template('index.html', students=data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
